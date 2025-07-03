ca7c79245e52788aebd7b99d7ef364783f316ccf
mikey weller <mikey.weller@terex.com>
2025-07-03 14:47:03 -0700

    update with upstream ros2 example

--- flake.nix
+++ flake.nix
@@ -12,10 +12,24 @@
       in
       {
         devShell = pkgs.mkShell {
-          buildInputs = [ ros.turtlesim
-                          ros.ros2run
-                          ros.rmw-fastrtps-dynamic-cpp ];
-          RMW_IMPLEMENTATION = "rmw_fastrtps_dynamic_cpp";
+          nativeBuildInputs = [
+            (buildEnv {
+              paths = [
+                ros-core
+                colcon
+                geometry-msgs
+                turtlebot4-desktop
+                turtlebot4-simulator
+                slam-toolbox
+                nav2-minimal-tb4-sim
+                nav2-minimal-tb3-sim
+                # rqt metapackages
+                rqt-common-plugins
+                rqt-tf-tree
+                tf2-tools
+              ];
+            })
+          ];
         };
       }
     );
